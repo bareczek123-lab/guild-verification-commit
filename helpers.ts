30f83492cb62765b26753c77a0abb87c006281bb
bareczek123-lab <bareczek123@gmail.com>
2026-03-16 20:51:24 +0100

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T19:51:15.816Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-16T19:51:17.252Z
+Commit: Fix CSS styling bug on mobile devices
