36877148f84b6e27e676c8eb0a19d04e9f4d1686
bareczek123-lab <bareczek123@gmail.com>
2026-03-16 20:50:35 +0100

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T19:47:44.262Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-16T19:50:28.381Z
+Commit: Fix CSS styling bug on mobile devices
